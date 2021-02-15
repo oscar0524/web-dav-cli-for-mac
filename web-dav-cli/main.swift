ddd016f7c700d72eb6b8d286a98e7baf53406deb
oscar0524 <oscar870524@gmail.com>
2021-02-15 17:06:36 +0800

    create demo app

--- web-dav-cli/main.swift
+++ web-dav-cli/main.swift
@@ -8,5 +8,13 @@
 
 import Foundation
 
-print("Hello, World!")
+let webdavEvent=WebDavEvent(url:"http://127.0.0.1",rootPath: "/webdav",user:"user",password: "test1234")
+//webdavEvent.ls()
+//webdavEvent.createFolder()
+//webdavEvent.remove(path:"/new folder")
+//webdavEvent.download(path: "/test1234.txt", localPath: NSHomeDirectory()+"/test1234.txt")
+//webdavEvent.upload(path: "/testupload.txt", localPath: NSHomeDirectory()+"/testupload.txt")
+//webdavEvent.move(path: "/test1234.txt", to: "/新增資料夾/test1234.txt")
+//webdavEvent.copy(path: "/test1234.txt", to: "/新增資料夾/test1234.txt")
 
+RunLoop.main.run()
